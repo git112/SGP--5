16676e9a0a7e15e92dc8ac8df46a0e6eb85c4852
Naseta <naseta121@gmail.com>
2026-03-05 09:48:37 +0530

    CI/CD pipeline

--- chatbot/main.py
+++ chatbot/main.py
@@ -18,7 +18,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://placementor-ai-two.vercel.app",
+        "http://localhost:8080",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
